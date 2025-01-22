87200d951d5bb9ca6e44671405d88d653db8bc56
FlyingHighNow <jakobmich63@gmail.com>
2025-01-22 17:29:31 -0600

    improved lighting and added movement controls

--- vite/main.js
+++ vite/main.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
 // Create scene
 const scene = new THREE.Scene();
@@ -10,11 +11,11 @@ camera.position.z = 5;
 
 //Create an object
 const geometry = new THREE.DodecahedronGeometry();
-const material = new THREE.MeshBasicMaterial({color: '#468585'});
+const material = new THREE.MeshLambertMaterial({color: '#468585', emissive: '#468585'});
 const dodecahedron = new THREE.Mesh(geometry, material);
 
 const boxGeometry = new THREE.BoxGeometry(2, 0.1, 2);
-const boxMaterial = new THREE.MeshBasicMaterial({color: '#468585'});
+const boxMaterial = new THREE.MeshBasicMaterial({color: '#B4B4B3'});
 const box = new THREE.Mesh(boxGeometry, boxMaterial);
 box.position.y = -1.5;
 
@@ -28,8 +29,31 @@ scene.add(light);
 
 //renderer
 const renderer = new THREE.WebGLRenderer({canvas});
-
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio);
 
 renderer.render(scene, camera);
 
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor = 0.05;
+controls.enableZoom = true;
+controls.enablePan = true;
+
+function animate() {
+    requestAnimationFrame(animate);
+
+    dodecahedron.rotation.x += 0.01;
+    dodecahedron.rotation.y += 0.01;
+
+    controls.update();
+    renderer.render(scene, camera);
+};
+
+window.addEventListener('resize', () => {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+});
+
+animate();
